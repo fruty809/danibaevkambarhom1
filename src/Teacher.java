d87cf27911361949d7e23577d8f6b32148d20092
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-06 19:37:44 +0600

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Teacher {
+public  final class Teacher extends HeadTeacher{
+    private String workExperience;
+
+    public Teacher(int age, String name, Salary salary, School school,String workExperience) {
+        super(age, name, salary, school);
+        this.workExperience = workExperience;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " Work experience: " + workExperience;
+    }
 }
