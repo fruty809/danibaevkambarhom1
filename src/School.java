7f6a35c1e0fab633d5c452de24a2e9ff042cbe4a
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-06 19:37:20 +0600

    Update School.java

--- src/School.java
+++ src/School.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class School {
+public class School {
+    private int numberSchool;
+    private String nameSchool;
+
+    public School(int numberSchool, String nameSchool){
+        this.numberSchool = numberSchool;
+        this.nameSchool = nameSchool;
+    }
+
+    public int getNumberSchool() {
+        return numberSchool;
+    }
+
+    public String getNameSchool() {
+        return nameSchool;
+    }
 }
