88595472c3bbb447bf5ad7ebef79ab6415e93556
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-06 19:36:01 +0600

    Update HeadTeacher.java

--- src/HeadTeacher.java
+++ src/HeadTeacher.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class HeadTeacher {
+public class HeadTeacher extends Director{
+    private int age;
+    private String name;
+
+    public HeadTeacher(int age, String name, Salary salary, School school){
+        super();
+        this.age = age;
+        this.name = name;
+        super.setSalary(salary);
+        super.setSchool(school);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getInfo(){
+        return "age: " + getAge() + " ; Name: "  + getName() +
+                " ; Salary: " + getSalary() + " ;  School Number: " + getSchool().getNumberSchool() + " : School Name: " + getSchool().getNameSchool() + " : ";
+    }
+
+    public void mood(){
+        System.out.println("Настроение хорощее");
+    }
+    public void mood(String mood){
+        System.out.println("Настроение: " + mood);
+    }
 }
