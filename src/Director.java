17232d9462f6f1e14ade692f9d3b0dee68ef3084
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-06 19:35:35 +0600

    Update Director.java

--- src/Director.java
+++ src/Director.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Director {
+public class Director {
+    private Salary salary;
+    private School school;
+
+
+    public void setSalary(Salary salary) {
+        this.salary = salary;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public Salary getSalary() {
+        return salary;
+    }
+
+    public School getSchool() {
+        return school;
+    }
 }
